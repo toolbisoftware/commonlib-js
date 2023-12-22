4f973363a7190410afae9636e0940fdc8058d09d
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-22 16:31:03 +0100

    Create index.ts

---
+++ src/functions/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+export * from "./getType.ts";
+export * from "./isEmpty.ts";
