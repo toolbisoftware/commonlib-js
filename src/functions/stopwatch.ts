f2a6f44d7a9567c295169227198445d7ea448f88
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-24 18:47:27 +0100

    Create stopwatch.ts

---
+++ src/functions/stopwatch.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+import type { StopwatchReturn } from "../types.ts";
+
+/**
+ * Creates a stopwatch. Usually used to measure the time a function takes to run.
+ *
+ * @param {number} [decimals] Number of decimals to use for the returned time. · `number`
+ * @default
+ * 2
+ * @return {StopwatchReturn} A new stopwatch with the following methods ({@link StopwatchReturn}):
+ * - `getTime`: Gets the time elapsed since the creation of the stopwatch. · `number`
+ * @version 1.0.0
+ */
+export function stopwatch(decimals?: number): StopwatchReturn {
+  const startTime = performance.now();
+  let decimals_: number | undefined = undefined;
+
+  const useDecimals = (decimals?: number) => {
+    if (decimals) {
+      decimals_ = decimals;
+      if (decimals_ < 0 || decimals_ > 20) {
+        throw new Error(
+          "Parameter 'decimals' must be a number between 0 and 20."
+        );
+      }
+    }
+  };
+
+  useDecimals(decimals);
+
+  function getTime(decimals?: number): number {
+    const stopTime = performance.now();
+    useDecimals(decimals);
+
+    return parseFloat((stopTime - startTime).toFixed(decimals_ || 2));
+  }
+
+  return {
+    getTime
+  };
+}
