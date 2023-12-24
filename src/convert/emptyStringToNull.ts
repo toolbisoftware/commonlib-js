71c8da9f69aaa15e1b7bfbf1e50fb7950fa356a1
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-24 16:52:09 +0100

    Create emptyStringToNull.ts

---
+++ src/convert/emptyStringToNull.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+import { inputCheckerType } from "../private/index.ts";
+
+/**
+ * Converts a string into `null` if it's empty.
+ *
+ * @alias strToNull
+ * @param string The string to convert. · `string`
+ * @returns {string | null} The provided string if it's not empty, `null` otherwise. · `string | null`
+ * @example
+ * import { emptyStringToNull } from "commonlib-js";
+
+ * const userName = "Hello";
+ * console.log(emptyStringToNull(userName)); // "Hello"
+
+ * const emptyUserName = "";
+ * console.log(emptyStringToNull(emptyUserName)); // null
+ */
+export function emptyStringToNull(string: string): string | null {
+  inputCheckerType([
+    {
+      value: string,
+      name: "string",
+      type: ["string"]
+    }
+  ]);
+
+  if (string.length) {
+    return string;
+  } else {
+    return null;
+  }
+}
+export const strToNull = emptyStringToNull;
