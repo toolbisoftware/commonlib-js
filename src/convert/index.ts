8468fa1f06b094713de074faa9507cd848561565
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-24 16:52:23 +0100

    Create index.ts

---
+++ src/convert/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+export * from "./booleanToNumber.ts";
+export * from "./emptyStringToNull.ts";
+export * from "./itemToArray.ts";
+export * from "./jsonStringToObject.ts";
+export * from "./numberToBoolean.ts";
+export * from "./timeToMs.ts";
