f456bf03075f007180ef68e6c61ef14ead85c09c
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-24 16:54:23 +0100

    Create itemToArray.ts

---
+++ src/convert/itemToArray.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+/**
+ * Converts an item into an array.
+ *
+ * @template T The type of the returned array.
+ * @param {T} item The item to convert. · `T`
+ * @returns {T[]} The item in an array. · `T[]`
+ * @example
+ * import { itemToArray } from "commonlib-js";
+
+ * const message = "hello";
+
+ * console.log(itemToArray<string>(message)); // ["hello"]
+ */
+export function itemToArray<T>(item: T | T[]): T[] {
+  if (Array.isArray(item)) {
+    return item;
+  } else {
+    return [item];
+  }
+}
