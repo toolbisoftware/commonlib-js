cd68172d5b91e4e016c0deaef8d03ceca358162f
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-24 16:57:51 +0100

    Create jsonStringToObject.ts

---
+++ src/convert/jsonStringToObject.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+import { inputCheckerType } from "../private/index.ts";
+
+/**
+ * Converts (parses) a string into an object.
+ *
+ * @template T (Optional) The type of the returned object.
+ * @param {string} jsonString The string to parse. · `string`
+ * @returns {T | null} An object if the string is parseable, `null` otherwise. · `T | null`
+ * @example
+ * import { jsonStringToObject } from "commonlib-js";
+
+ * const jsonString = '{"hello":"world"}';
+
+ * console.log(jsonStringToObject(jsonString)); // {hello: "world"}
+ * @version 1.0.0
+ */
+export function jsonStringToObject<T = object>(jsonString: string): T | null {
+  inputCheckerType([
+    {
+      value: jsonString,
+      name: "jsonString",
+      type: ["string"]
+    }
+  ]);
+
+  try {
+    const parse = JSON.parse(jsonString);
+    return parse;
+  } catch (err) {
+    return null;
+  }
+}
+export const jsonStrToObj = jsonStringToObject;
