db27ac8889b7e750a2db755e5f901b0d57f718c5
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-26 16:57:42 +0100

    Update types.ts

--- src/private/types.ts
+++ src/private/types.ts
@@ -3,13 +3,18 @@
 
 import type * as Types from "../types.ts";
 
-export type Utils_InputCheckerAdvancedType = {
+export type Utils_InputCheckerTypeAdvancedType = {
   name: string;
   values: any[];
 };
-export interface Utils_InputCheckerInput {
+
+export interface Utils_InputCheckerTypeInput {
   value: any;
   name: string;
-  type: (Types.ExtDataTypes | Utils_InputCheckerAdvancedType)[];
-  allowEmpty?: boolean;
+  type: (Types.ExtDataTypes | Utils_InputCheckerTypeAdvancedType)[];
+}
+
+export interface Utils_InputCheckerEmptyInput {
+  value: any;
+  name: string;
 }
