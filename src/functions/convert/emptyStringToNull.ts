fc09dca3d7c96c777ed516145e049128f47ef656
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-26 14:32:42 +0100

    Update emptyStringToNull.ts

--- src/functions/convert/emptyStringToNull.ts
+++ src/functions/convert/emptyStringToNull.ts
@@ -4,17 +4,17 @@
 import { inputCheckerType } from "../../private/index.ts";
 
 /**
- * Converts a string into `null` if it's empty.
+ * Converts a string into `null` if it's empty, returns the string otherwise.
  *
  * @alias strToNull
- * @param string The string to convert. · `string`
- * @returns {string | null} The provided string if it's not empty, `null` otherwise. · `string | null`
+ * @param {string} string (`string`) The string to convert.
+ * @returns {string | null} (`string | null`) `null` if the provided string is empty, the provided string otherwise.
  * @example
  * import { emptyStringToNull } from "commonlib-js";
-
- * const userName = "Hello";
- * console.log(emptyStringToNull(userName)); // "Hello"
-
+ *
+ * const userName = "Roberto";
+ * console.log(emptyStringToNull(userName)); // "Roberto"
+ *
  * const emptyUserName = "";
  * console.log(emptyStringToNull(emptyUserName)); // null
  * @version 1.0.0
@@ -34,4 +34,5 @@ export function emptyStringToNull(string: string): string | null {
     return null;
   }
 }
+
 export const strToNull = emptyStringToNull;
