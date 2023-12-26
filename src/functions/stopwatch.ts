23b49b157474427cfb0edf400fdad3b66c1a62ee
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-26 16:49:41 +0100

    Update stopwatch.ts

--- src/functions/stopwatch.ts
+++ src/functions/stopwatch.ts
@@ -1,29 +1,38 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
+import { inputCheckerType } from "../private/index.ts";
 import type { StopwatchReturn } from "../types.ts";
 
 /**
- * Creates a stopwatch. Usually used to measure the time a function takes to run.
+ * Creates a stopwatch.
  *
- * @param {number} [decimals] Number of decimals to use for the returned time. · `number`
- * @default
- * 2
- * @return {StopwatchReturn} A new stopwatch with the following methods ({@link StopwatchReturn}):
- * - `getTime`: Gets the time elapsed since the creation of the stopwatch. · `number`
+ * Usually used to measure the time that a function takes to run.
+ *
+ * @param {number} [decimals] [`number`] Number of decimals to use for the returned time.
+ * @returns {StopwatchReturn} ({@link StopwatchReturn}) A new stopwatch with the following methods:
+ * - `getTime` -> `number`: Gets the time elapsed since the creation of the stopwatch.
  * @example
  * import { stopwatch } from "commonlib-js";
-
+ *
  * const useStopwatch = stopwatch();
-
+ *
  * ...
  * @version 1.0.0
  */
 export function stopwatch(decimals?: number): StopwatchReturn {
+  inputCheckerType([
+    { value: decimals, name: "decimals", type: ["number", "undefined"] }
+  ]);
+
   const startTime = performance.now();
   let decimals_: number | undefined = undefined;
 
   const useDecimals = (decimals?: number) => {
+    inputCheckerType([
+      { value: decimals, name: "decimals", type: ["number", "undefined"] }
+    ]);
+
     if (decimals) {
       decimals_ = decimals;
       if (decimals_ < 0 || decimals_ > 20) {
