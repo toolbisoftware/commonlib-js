577714a891b1217c4c2ef1b577390ea3fc11bf26
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-26 16:06:38 +0100

    Update getType.test.ts

--- src/functions/getType.test.ts
+++ src/functions/getType.test.ts
@@ -3,29 +3,52 @@
 
 import { getType } from "./getType.ts";
 
-test("functions/getType · string", () => {
-  const type = getType("hello world");
-  expect(type).toMatch("string");
+test("functions/getType · bigint", () => {
+  const run = getType(BigInt(1));
+  expect(run).toMatch("bigint");
+});
+
+test("functions/getType · boolean", () => {
+  const run = getType(true);
+  expect(run).toMatch("boolean");
+});
+
+test("functions/getType · function", () => {
+  const run = getType(() => {});
+  expect(run).toMatch("function");
+});
+
+test("functions/getType · number", () => {
+  const run = getType(1);
+  expect(run).toMatch("number");
 });
 
 test("functions/getType · object", () => {
-  const type = getType({
-    hello: "world"
-  });
-  expect(type).toMatch("object");
+  const run = getType({ hello: "world" });
+  expect(run).toMatch("object");
 });
 
-test("functions/getType · array", () => {
-  const type = getType(["hello", "world"]);
-  expect(type).toMatch("array");
+test("functions/getType · string", () => {
+  const run = getType("hello world");
+  expect(run).toMatch("string");
 });
 
-test("functions/getType · null", () => {
-  const type = getType(null);
-  expect(type).toMatch("null");
+test("functions/getType · symbol", () => {
+  const run = getType(Symbol("hello world"));
+  expect(run).toMatch("symbol");
 });
 
 test("functions/getType · undefined", () => {
-  const type = getType(undefined);
-  expect(type).toMatch("undefined");
+  const run = getType(undefined);
+  expect(run).toMatch("undefined");
+});
+
+test("functions/getType · null", () => {
+  const run = getType(null);
+  expect(run).toMatch("null");
+});
+
+test("functions/getType · array", () => {
+  const run = getType(["hello world"]);
+  expect(run).toMatch("array");
 });
