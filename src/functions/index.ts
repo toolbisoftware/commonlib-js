ebeca812533e4a591866c292e690561a34635cf4
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-27 18:40:13 +0100

    Update index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,13 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
+export * from "./Logger.ts";
+export * from "./checkInput.ts";
+export * from "./checkType.ts";
+export * from "./convert/index.ts";
+export * from "./getFileNameInfo.ts";
+export * from "./getPaths.ts";
 export * from "./getType.ts";
 export * from "./isEmpty.ts";
+export * from "./mergePaths.ts";
+export * from "./stopwatch.ts";
