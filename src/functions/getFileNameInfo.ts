fa88226ba774f7bed631d07f474d8ebdca03338a
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-27 18:56:38 +0100

    Create getFileNameInfo.ts

---
+++ src/functions/getFileNameInfo.ts
@@ -0,0 +1,41 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+import { GetFileNameInfoReturn } from "../types.ts";
+
+/**
+ * Gets information about a file name.
+ *
+ * @param {string} fileName (`string`) The file name to get the information from.
+ * @returns {GetFileNameInfoReturn} ({@link GetFileNameInfoReturn}) An object with the information of the provided file name.
+ * @example
+ * import { getFileNameInfo } from "commonlib-js";
+ *
+ * const fileName = "index.ts";
+ * const getInfo = getFileNameInfo(fileName);
+ *
+ * console.log(getInfo);
+ * // {
+ * //   full: 'index.ts',
+ * //   name: 'index',
+ * //   ext: 'ts'
+ * // }
+ * @version 1.0.0
+ */
+export function getFileNameInfo(fileName: string): GetFileNameInfoReturn {
+  const result: GetFileNameInfoReturn = {
+    full: fileName,
+    name: "",
+    ext: ""
+  };
+
+  const lastDotIndex = fileName.lastIndexOf(".");
+  if (lastDotIndex === -1) {
+    result.name = fileName;
+  } else {
+    result.name = fileName.slice(0, lastDotIndex);
+    result.ext = fileName.slice(lastDotIndex + 1);
+  }
+
+  return result;
+}
