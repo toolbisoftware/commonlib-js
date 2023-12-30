778348f8c65a96fb4e7dac36953d393b9f8e911a
NEXZ <76069669+soynexz@users.noreply.github.com>
2023-12-30 17:14:55 +0100

    Update sar.ts

--- src/functions/express/sar.ts
+++ src/functions/express/sar.ts
@@ -1,15 +1,43 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-import express from "express";
+import { Response } from "express";
 import type {
   Express_SarOptions,
   Express_SarResponse,
   HttpCodes
 } from "../../types.ts";
 
+/**
+ * Sends a response to a request.
+ *
+ * @param {Response} res ({@link Response}) The request to send the response to.
+ * @param {HttpCodes} httpCode ({@link HttpCodes}) HTTP code to send the response with.
+ * @param {Express_SarOptions} [options] [{@link Express_SarOptions}] Options for the function.
+ * @default
+ * undefined
+ * @param {Express_SarOptions["code"]} [options.code] [`number`] Internal code of the response.
+ * @default
+ * undefined
+ * @param {Express_SarOptions["messageId"]} [options.messageId] [`string`] Internal id of the response's message. Use only alongside `options.message`.
+ * @default
+ * undefined
+ * @param {Express_SarOptions["message"]} [options.message] [`string`] Message to send. Use only alongside `options.messageId`.
+ * @default
+ * undefined
+ * @param {Express_SarOptions["content"]} [options.content] [`object`] Content to send.
+ * @default
+ * undefined
+ * @example
+ * import { sar } from "commonlib-js";
+ *
+ * ...
+ *
+ * sar(res, 200);
+ * @version 1.0.0
+ */
 export function sar(
-  res: express.Response,
+  res: Response,
   httpCode: HttpCodes,
   options?: Express_SarOptions
 ): void {
