a1ae8019b32f3455255e267000c1d15eb7336b06
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-05 19:06:07 +0100

    fix(functions/returnHandler): Make generics optional

--- src/types.ts
+++ src/types.ts
@@ -423,8 +423,8 @@ export interface ReturnHandlerObject<
 }
 
 export interface ReturnHandlerReturn<
-  CODES extends string | number,
-  CONTENT extends object | null
+  CODES extends string | number = string | number,
+  CONTENT extends object | null = null
 > {
   get: () => ReturnHandlerObject<CODES, CONTENT>;
   done: (
